6485ce0d3d29eecb5430620065b0bdd7dcf432d1
Amruth Venkatraman <amruth@squareup.com>
2015-07-14 13:37:38 -0700

    allow a passed svm file to be used for classification

--- src/classify_func.py
+++ src/classify_func.py
@@ -4,19 +4,25 @@ import sys
 from skimage import io
 import numpy as np
 
-detector = dlib.simple_object_detector("square_bottle_classifier.svm")
+test_images_dir = '../test_images/'
+default_svm_param_file = "square_bottle_classifier.svm"
 
-assorted_dir = '../test_images/'
+items =os.listdir(test_images_dir)
 
-items =os.listdir(assorted_dir)
+def classify(dir, svm_param_file):
+    detector = dlib.simple_object_detector(svm_param_file)
+    for dirr, _, files in os.walk(test_images_dir):
+        sorted_files = sorted(files)
+        for f in files:
+            if f.endswith('.jpeg'):
+                im = io.imread(dirr + f)
+                dets = detector(im)
+                print f + ' had: ', len(dets)
 
-def classify(dir):
-  for dirr, _, files in os.walk(assorted_dir):
-    sorted_files = sorted(files)
-    for f in files:
-      if f.endswith('.jpeg'):
-        im = io.imread(dirr + f)
-        dets = detector(im)
-        print f + ' had: ', len(dets)
 if __name__ == '__main__':
-  classify(assorted_dir)
+    print sys.argv
+    if len(sys.argv) == 2:
+        classify(test_images_dir, sys.argv[1])
+    else:
+        #use default svm file
+        classify(test_images_dir, default_svm_param_file)
