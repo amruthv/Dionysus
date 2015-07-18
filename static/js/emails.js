cde3bf7380278362596a0eaba42c88b80ca3a9e9
Amruth Venkatraman <amruth@squareup.com>
2015-07-17 17:09:49 -0700

    emails form

---
+++ static/js/emails.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#submit_btn').on('click', (function() {
+        option = $('#emailOption').val()
+        email = $('#emailToOperate').val();
+        if (option == "add") {
+            $.post("/addemail?email="+email);
+        }
+        else if (option == "remove") {
+            $.post("/removeemail?email=" + email);
+        }
+        window.location = 'emails.html'
+    }));
+})
